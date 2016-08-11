6ca08615be4ee01d92c504099115724b5725b84c
Tameem Syed <tsyed@lumeris.com>
2016-08-11 09:54:03 -0500

    modified

--- spark/src/main/java/com/demo/spark/App.java
+++ spark/src/main/java/com/demo/spark/App.java
@@ -7,24 +7,20 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
+public class App {
+	public static void main(String[] args) {
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	SparkConf conf = new SparkConf().setAppName("AWS EMR Demo");
-    	JavaSparkContext sc = new JavaSparkContext(conf);
-    	
-		List<String> strings = Arrays.asList("Hello", "World");
-		JavaRDD<String> s1 = sc.parallelize(strings);
-		List<String> s = s1.collect();
-		
-		for (String ss : s) {
-			
-			System.out.println(ss);
-		}
-		
+		System.out.println("Hello World");
+		// SparkConf conf = new SparkConf().setAppName("AWS EMR Demo");
+		// JavaSparkContext sc = new JavaSparkContext(conf);
 
-		
-    }
+		// List<String> strings = Arrays.asList("Hello", "World");
+		// JavaRDD<String> s1 = sc.parallelize(strings);
+		// List<String> s = s1.collect();
+		//
+		// for (String ss : s) {
+
+		// }
+
+	}
 }
