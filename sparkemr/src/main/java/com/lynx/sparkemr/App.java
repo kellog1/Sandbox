d7c5976ee317330a51d8e4aa9235ecc0602ac35c
Tameem Syed <kellog1@gmail.com>
2016-08-11 16:17:47 -0500

    Version 0.1

---
+++ sparkemr/src/main/java/com/lynx/sparkemr/App.java
@@ -0,0 +1,19 @@
+package com.lynx.sparkemr;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+public class App {
+	public static void main(String[] args) {
+		SparkConf conf = new SparkConf().setAppName("Simple Application");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+
+		List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
+		JavaRDD<Integer> distData = sc.parallelize(data);
+		distData.count();
+	}
+}
